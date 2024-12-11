78c2dec420800060df84600046b6f7f00661bcbe
gomid-papi <owenleigh.gomid@nmsc.edu.ph>
2024-12-11 15:30:20 +0800

    bsan unsa

---
+++ submit_feedback.php
@@ -0,0 +1,32 @@
+<?php
+// submit_feedback.php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "feedback_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $feedback = $_POST['feedback'];
+
+  $sql = "INSERT INTO feedback (name, email, feedback) VALUES ('$name', '$email', '$feedback')";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Feedback submitted successfully!";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+}
+
+$conn->close();
+?>
